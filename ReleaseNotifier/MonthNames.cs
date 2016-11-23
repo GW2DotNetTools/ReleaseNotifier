7c6a3689ab9f1d265d93b4400231d0ed8acfed45
Dreanor <mikeundead@googlemail.com>
2016-11-23 23:48:57 +0100

    init project

---
+++ ReleaseNotifier/MonthNames.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace ReleaseNotifier
+{
+    public static class MonthNames
+    {
+        public readonly static List<string> GermanMonths = new List<string>
+        {
+            "Januar",
+            "Februar",
+            "März",
+            "April",
+            "Mai",
+            "Juni",
+            "Juli",
+            "August",
+            "September",
+            "Oktober",
+            "November",
+            "Dezember"
+        };
+
+        public readonly static List<string> EnglishMonths = new List<string>
+        {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        };
+
+        public readonly static List<string> SpanishMonths = new List<string>
+        {
+            "enero",
+            "febrero",
+            "marzo",
+            "abril",
+            "mayo",
+            "junio",
+            "julio",
+            "agosto",
+            "septiembre",
+            "octubre",
+            "noviembre",
+            "diciembre"
+        };
+
+        public readonly static List<string> FrenchMonths = new List<string>
+        {
+            "janvier",
+            "février",
+            "mars",
+            "avril",
+            "mai",
+            "juin",
+            "juillet",
+            "août",
+            "septembre",
+            "octobre",
+            "novembre",
+            "décembre"
+        };
+    }
+}
---
+++ ReleaseNotifier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReleaseNotifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var day = DateTime.Now.Day;
+            var month = DateTime.Now.Month - 1;
+            var year = DateTime.Now.Year;
+
+            string en = $"https://forum-en.guildwars2.com/forum/info/updates/Game-Update-Notes-{MonthNames.EnglishMonths[month]}-{day}-{year}";
+            string fr = $"https://forum-fr.guildwars2.com/forum/info/updates/Mise-jour-du-jeu-{day}-{MonthNames.FrenchMonths[month]}-{year}";
+            string de = $"https://forum-de.guildwars2.com/forum/info/updates/Release-Notes-zum-Spiel-{day}-{MonthNames.GermanMonths[month]}-{year}";
+            string es = $"https://forum-es.guildwars2.com/forum/info/updates/Notas-de-actualizaci-n-del-juego-{day}-{MonthNames.SpanishMonths[month]}-{year}";
+
+            Parallel.Invoke(
+                () => Connect(en),
+                () => Connect(fr),
+                () => Connect(de),
+                () => Connect(es));
+        }
+
+        private static void Connect(string url)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString()}] Checking: {url}");
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "HEAD";
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            if (response.ResponseUri.ToString() == url)
+            {
+                response.Close();
+                Process.Start(url);
+            }
+            else
+            {
+                Console.WriteLine($"[{DateTime.Now.ToString()}] Response does not match. ({response.ResponseUri})");
+                response.Close();
+                Thread.Sleep(10000);
+                Connect(url);
+            }
+        }
+    }
+}
